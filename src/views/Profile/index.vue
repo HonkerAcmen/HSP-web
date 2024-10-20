e4d19dde4ea07a902f621ba90959e7abefe18271
Mark Yang <haker0x04@163.com>
2024-10-20 18:10:15 +0800

    调用了后端获取信息接口，并协调

--- src/views/Profile/index.vue
+++ src/views/Profile/index.vue
@@ -6,29 +6,16 @@
         <div class="profile-top">
             <div class="avatar"><el-avatar :icon="UserFilled" /></div>
             <div class="title">
-                <span>爱媳妇的老猪</span><span>性别: 男 年龄: 18</span
-                ><span>haker0x04@163.com</span><span>天工若拙，大巧若通</span>
+                <span>{{ userinfo.userName }}</span><span>性别: {{ userinfo.gender }} </span><span>{{userinfo.email}}</span><span>{{ userinfo.userDesc }}</span>
             </div>
             <div class="button">修改个人信息</div>
         </div>
 
         <!-- 下部分，加入的课程 -->
         <div class="profile-down">
-            <ul
-                v-infinite-scroll="loadData"
-                :infinite-scroll-disabled="
-                    loadedCourses.length >= testCourseData.length
-                "
-                :infinite-scroll-distance="10"
-                class="infinite-list"
-                style="overflow: auto"
-            >
-                <li
-                    v-for="i in loadedCourses"
-                    :key="i.courseName"
-                    class="infinite-list-item"
-                    @click=""
-                >
+            <ul v-infinite-scroll="loadData" :infinite-scroll-disabled="loadedCourses.length >= testCourseData.length"
+                :infinite-scroll-distance="10" class="infinite-list" style="overflow: auto">
+                <li v-for="i in loadedCourses" :key="i.courseName" class="infinite-list-item" @click="">
                     <h2>{{ i.courseName }}</h2>
                     <h6>教师: {{ i.coursePerson }}</h6>
                     <h6>{{ i.courseDesc }}</h6>
@@ -40,21 +27,68 @@
 
 <script lang="ts" setup>
 import HeaderNav from "@/components/HeaderNav.vue";
-import Breadcrumb from "@/components/Breadcrumb.vue"
+import Breadcrumb from "@/components/Breadcrumb.vue";
 import { UserFilled } from "@element-plus/icons-vue";
 import { type userCourse, testCourseData } from "@/res/dataModel";
-import { ref } from "vue";
+import { ref, onMounted } from "vue";
 import axios from "axios";
-import {ServerAddress} from "@/utils/serverURL"
+import { ServerAddress } from "@/utils/serverURL";
+import { ElMessage } from "element-plus";
 
+// 处理登出
 function headleLogout() {
     localStorage.clear();
     window.location.reload();
 }
 
+// 响应式用户信息
+const userinfo = ref({
+    email: "",
+    userName: "",
+    gender: "",
+    profileImg: "",
+    userDesc: ""
+});
+
+// 获取用户信息
 async function GetUserProfile() {
-    await axios.get(ServerAddress+"/getUserInfo?")
+    try {
+        const res = await axios.get(ServerAddress + "/api/getUserInfo/" + localStorage.getItem('token'));
+        if (res.data.code !== 200) {
+            ElMessage({
+                message: res.data.message,
+                type: 'error'
+            });
+            return;
+        } else {
+            localStorage.setItem("userdata", JSON.stringify(res.data.data)); // 存储数据
+            userinfo.value = {
+                email: res.data.data.email || "无",
+                userName: res.data.data.userName || "无名氏",
+                gender: res.data.data.gender || "不明",
+                profileImg: res.data.data.profileImg || "", // 添加这一行
+                userDesc: res.data.data.userDesc || "来写点什么，彰显你的个性吧"
+            };
+        }
+    } catch (err: any) {
+        console.error(err); // 记录错误
+    }
 }
+// 在组件挂载时获取用户信息
+onMounted(() => {
+    if (localStorage.getItem("userdata") == null) {
+        GetUserProfile();
+    } else {
+        const userdata = JSON.parse(localStorage.getItem("userdata") || '{}');
+        userinfo.value = {
+            email: userdata.email || "无",
+            userName: userdata.userName || "无名氏",
+            gender: userdata.gender || "不明",
+            profileImg: userdata.profileImg || "", // 添加这一行
+            userDesc: userdata.userDesc || "来写点什么，彰显你的个性吧"
+        };
+    }
+});
 
 // 懒加载代码
 const count = 6; // 每次加载6个数据
@@ -75,12 +109,11 @@ const loadData = () => {
     }
 };
 
-
-//  面包屑的路径
+// 面包屑的路径
 const breadcrumbItems = ref([
-    {text : '主页', link : '/'},
-    {text : '个人资料', link : '/profile'}
-])
+    { text: '主页', link: '/' },
+    { text: '个人资料', link: '/profile' }
+]);
 
 // 初始化数据
 initializeData();
